9f6e5443f710f6b5a26e4a2d66c0eb4c472318a0
Mack Ward <mackorone@gmail.com>
2022-03-18 06:16:51 -0400

    Refactor committer.py

--- committer.py
+++ committer.py
@@ -11,9 +11,6 @@ logger: logging.Logger = logging.getLogger(__name__)
 
 
 class Committer:
-    NAME = "github-actions[bot]"
-    EMAIL = "github-actions[bot]@users.noreply.github.com"
-
     @classmethod
     def commit_and_push_if_github_actions(cls) -> None:
         github_actions = cls._get_env("GITHUB_ACTIONS")
@@ -21,30 +18,38 @@ class Committer:
             logger.info("Not GitHub Actions, skipping")
             return
 
-        result = cls._run(["git", "status", "-s"])
+        run_number = cls._get_env("GITHUB_RUN_NUMBER")
+        event_name = cls._get_env("GITHUB_EVENT_NAME")
+        cls.commit_and_push(
+            commit_message=f"Run {run_number} via {event_name}",
+            user_name="github-actions[bot]",
+            user_email="github-actions[bot]@users.noreply.github.com",
+        )
+
+    @classmethod
+    def commit_and_push(
+        cls,
+        commit_message: str,
+        user_name: Optional[str] = None,
+        user_email: Optional[str] = None,
+    ) -> None:
+        result = cls._run(["git", "status", "--short"])
         any_changes = bool(result.stdout)
         if not any_changes:
             logger.info("No changes, skipping")
             return
 
         logger.info("Staging changes")
-        cls._run(["git", "add", "-A"])
+        cls._run(["git", "add", "--all"])
 
         logger.info("Committing changes")
-        run_number = cls._get_env("GITHUB_RUN_NUMBER")
-        event_name = cls._get_env("GITHUB_EVENT_NAME")
-        cls._run(
-            [
-                "git",
-                "-c",
-                f"user.name={cls.NAME}",
-                "-c",
-                f"user.email={cls.EMAIL}",
-                "commit",
-                "-m",
-                f"Run {run_number} via {event_name}",
-            ]
-        )
+        args = ["git"]
+        if user_name:
+            args += ["-c", f"user.name={user_name}"]
+        if user_email:
+            args += ["-c", f"user.email={user_email}"]
+        args += ["commit", "-m", commit_message]
+        cls._run(args)
 
         logger.info("Pushing changes")
         cls._run(["git", "push"])
@@ -56,5 +61,5 @@ class Committer:
 
     @classmethod
     @external
-    def _run(cls, args: Sequence[str]) -> subprocess.CompletedProcess:
+    def _run(cls, args: Sequence[str]) -> "subprocess.CompletedProcess[str]":
         return subprocess.run(args=args, capture_output=True, check=True, text=True)
